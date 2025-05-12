fc6dca34a3432a05580dd81389bb8d13ba3edb68
Joachim Pihlgren <j.pihlgren@outlook.com>
2025-05-12 23:07:57 +0200

    add skills

--- src/app/shared/components/skills/skills.component.ts
+++ src/app/shared/components/skills/skills.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
+import { Skill, SkillsService } from './services/skills.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-skills',
@@ -7,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
+  protected skillsService: SkillsService = inject(SkillsService)
+  skills: Signal<Skill[] | undefined>
+  placeholderSkills: string[] = new Array(6).fill("")
 
+  constructor() {
+    this.skills = toSignal(this.skillsService.skills)
+  }
 }
