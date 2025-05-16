fcd58f8277eb1f65ae3e29537f5ed452a71f5bde
Joachim Pihlgren <j.pihlgren@outlook.com>
2025-05-16 21:07:28 +0200

    write

--- src/app/shared/components/experience/services/experience.service.ts
+++ src/app/shared/components/experience/services/experience.service.ts
@@ -7,12 +7,12 @@ import { Observable, of } from 'rxjs';
 export class ExperienceService {
 
   private _experiences: Experience[] = [
-    { title: "Product Owner and designer", type: "job", workplace: "Värderingsdata AB", city: "Göteborg", country: "Sweden", start: { year: 2025, month: 3, day: 1 }, description: "" },
-    { title: "User Experience Designer", type: "job", workplace: "Värderingsdata AB", city: "Göteborg", country: "Sweden", start: { year: 2021, month: 10, day: 1 }, end: { year: 2025, month: 3, day: 1 }, description: "" },
-    { title: "User Experience Designer", type: "job", workplace: "Värderingsdata AB via Nexer Group", city: "Göteborg", country: "Sweden", start: { year: 2020, month: 11, day: 1 }, end: { year: 2021, month: 11, day: 1 }, description: "" },
-    { title: "Interaktions Design, Tek. Mag.", type: "education", workplace: "Chalmers Tekniska Högskola", city: "Göteborg", country: "Sweden", start: { year: 2018, month: 8, day: 1 }, end: { year: 2020, month: 7, day: 1 }, description: "" },
-    { title: "Kognitionsvetenskap, Fil. Kand.", type: "education", workplace: "Värderingsdata AB", city: "Göteborg", country: "Sweden", start: { year: 2015, month: 8, day: 1 }, end: { year: 2018, month: 7, day: 1 }, description: "" },
-
+    productOwner,
+    userExperienceDesignerVarderingsdata,
+    userExperienceDesignerNexer,
+    mastersThesis,
+    interactionDesign,
+    cognitiveScienve
   ]
   constructor() { }
 
@@ -39,3 +39,72 @@ export interface Experience {
   },
   description: string
 }
+const productOwner: Experience = {
+  title: "Product Owner and designer",
+  type: "job",
+  workplace: "Värderingsdata AB",
+  city: "Göteborg",
+  country: "Sweden",
+  start: { year: 2025, month: 3, day: 1 },
+  description: ""
+}
+const userExperienceDesignerVarderingsdata: Experience = {
+  title: "User Experience Designer",
+  type: "job",
+  workplace: "Värderingsdata AB",
+  city: "Göteborg",
+  country: "Sweden",
+  start: {
+    year: 2021, month: 10, day: 1
+  },
+  end: {
+    year: 2025, month: 3, day: 1
+  },
+  description: ""
+}
+const userExperienceDesignerNexer: Experience = {
+  title: "User Experience Designer",
+  type: "job",
+  workplace: "Värderingsdata AB via Nexer Group",
+  city: "Göteborg",
+  country: "Sweden",
+  start: { year: 2020, month: 11, day: 1 },
+  end: { year: 2021, month: 11, day: 1 },
+  description: ""
+}
+const mastersThesis: Experience = {
+  title: `Masterexamensarbete: Cross-Device Interaction With Meta Space
+Designing Meaningful Interaction with Building Information Models across Devices`,
+  type: "job",
+  workplace: "Sweco, i samarbete med Chalmers Tekniska Högskola",
+  city: "Göteborg",
+  country: "Sweden",
+  start: { year: 2020, month: 1, day: 1 },
+  end: { year: 2020, month: 7, day: 1 },
+  description: `Undersökte hur forskningsläget inom Cross-device Interaction kan tillämpas i bygginformationskontexten. Arbetade praktiskt med design av gränssnitt för Swecos BIM-verktyg för samarbete mellan enheter (mobil, surfplatta, desktop).
+Genomförde Research Through Design och tog fram ett ramverk samt designriktlinjer för tvärenhetsinteraktion i professionella miljöer. Arbetet belyser begränsningar i befintliga interaktionstekniker och behovet av praktiskt anpassade lösningar i företagssammanhang.`
+}
+const interactionDesign: Experience = {
+  title: "Interaktions Design, Tek.Mag.",
+  type: "education",
+  workplace: "Chalmers Tekniska Högskola",
+  city: "Göteborg",
+  country: "Sweden",
+  start: { year: 2018, month: 8, day: 1 },
+  end: { year: 2020, month: 7, day: 1 },
+  description: `Tvåårigt program med fokus på användarcentrerad design, interaktiv teknik och prototyputveckling. Jag har arbetat med hela designprocessen — från research och konceptutveckling till prototyping och användartester — i projekt med fokus på UX, spelmekanik, fysiska gränssnitt och informationsvisualisering.
+Har använt verktyg som JavaScript, Figma och Unity, och fördjupat mig i områden som mobil interaktion, gameplay design, grafiska gränssnitt och tangible interaction.`
+}
+
+const cognitiveScienve: Experience = {
+  title: "Kognitionsvetenskap, Fil. Kand.",
+  type: "education",
+  workplace: "Värderingsdata AB",
+  city: "Göteborg",
+  country: "Sweden",
+  start: { year: 2015, month: 8, day: 1 },
+  end: { year: 2018, month: 7, day: 1 },
+  description: ""
+}
+
+
